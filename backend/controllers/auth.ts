c2cdca1806c566db685a73e091d82bdfaf993673
ayush00git <ayush.2007.iit@gmail.com>
2025-12-14 18:07:17 +0530

    feat: change password via old password

--- backend/controllers/auth.ts
+++ backend/controllers/auth.ts
@@ -36,23 +36,62 @@ export const handleUserSignUp = async (req: Request, res: Response) => {
 };
 
 export const handleUserLogIn = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  const existingUser = await Auth.findOne({ email });
-  if(!existingUser) {
-    return res.status(400).json({ "message": "You don't have an account" });
-  }
+  try {
+    const { email, password } = req.body;
+    const existingUser = await Auth.findOne({ email });
+    
+    if(!existingUser) {
+      return res.status(400).json({ "message": "You don't have an account" });
+    }
 
-  const inputhash = hashPassword(password, existingUser.salt);
-  if(inputhash != existingUser.password) {
-    return res.status(400).json({ "message": "Incorrect password" })
-  }
-  const token = generateToken(existingUser);
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: false,
-    sameSite: 'lax',
-    maxAge: 24 * 60 * 60 * 1000,
-  });
+    const inputhash = hashPassword(password, existingUser.salt);
+  
+    if(inputhash !== existingUser.password) {
+      return res.status(400).json({ "message": "Incorrect password" })
+    }
+    
+    const token = generateToken(existingUser);
+  
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+      maxAge: 24 * 60 * 60 * 1000,
+    });
 
-  return res.status(200).json({ "message": "Logged in success" });
+    return res.status(200).json({ "message": "Logged in success" });
+  } catch (error) {
+    console.log(`${error}`);
+    throw new Error(`While logging in`);
+  }
 };
+
+// via old password
+export const handleForgetPass = async(req: Request, res: Response) => {
+  try {
+    const { email, oldPassword, newPassword } = req.body;
+    const user = await Auth.findOne({ email });
+
+    if(!user) {
+      return res.status(400).json({ "message": "Account with that email does not exists" });
+    }
+
+    const oldHash = hashPassword(oldPassword, user?.salt);
+
+    if(oldHash !== user?.password) {
+      return res.status(400).json({ "message": "You have entered a wrong password" });
+    }
+
+    const newSalt = generateSalt();
+    const newHash = hashPassword(newPassword, newSalt);
+
+    user.password = newHash;
+    user.salt = newSalt;
+    await user.save();
+    return res.status(200).json({ "message": "Password changed successfully" });
+
+  } catch (error) {
+    console.log(`${error}`)
+    throw new Error(`While changing the password`);
+  }
+}
--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { handleUserSignUp, handleUserLogIn } from "../controllers/auth";
+import { handleUserSignUp, handleUserLogIn, handleForgetPass } from "../controllers/auth";
 
 export const authRoute = express.Router();
 
 authRoute.post('/signup', handleUserSignUp);
 authRoute.post('/login', handleUserLogIn);
+authRoute.post('/forget-password/viaOldPass', handleForgetPass);
