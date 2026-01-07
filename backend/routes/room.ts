9a740cb7e8250f5c1fd1cab199721499f90937d1
ayush00git <ayush.2007.iit@gmail.com>
2026-01-07 21:13:36 +0530

    typo-fix

--- backend/routes/room.ts
+++ backend/routes/room.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { handleCreateRoom, handleRoomInfo, handleJoining, handleJoinedRooms, handleGetAllRooms } from "../controllers/room";
-import { allowOnlyAuthenticatedUser } from "../middlewares/auth.ts";
+import { allowOnlyAuthenticatedUser } from "../middlewares/auth";
 
 export const roomRoute = express.Router();
 
 roomRoute.get('/allRooms', handleGetAllRooms);
-roomRoute.post('/', allowOnlyAutheticatedUser, handleCreateRoom);
+roomRoute.post('/', allowOnlyAuthenticatedUser, handleCreateRoom);
 roomRoute.get('/joined', allowOnlyAuthenticatedUser, handleJoinedRooms);
 roomRoute.get('/:roomId/join', allowOnlyAuthenticatedUser, handleJoining);
 roomRoute.get('/:roomId', allowOnlyAuthenticatedUser, handleRoomInfo);
