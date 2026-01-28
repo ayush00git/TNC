031e299ac3fc8bba77f4ece69856fb558d51a331
ayush00git <ayush.2007.iit@gmail.com>
2026-01-29 00:16:19 +0530

    feat(edit): added edit api

--- backend/controllers/project.ts
+++ backend/controllers/project.ts
@@ -87,3 +87,39 @@ export const deleteProjectById = async(req: Request, res: Response) => {
   }
 };
 
+export const editProjectById = async(req: Request, res: Response) => {
+  const { projectId } = req.params;
+  const userId = req.user?._id;
+
+  try {
+    const { title, description, tags, githubLink, liveLink } = req.body;
+
+    if ( !title || !description || !githubLink ) {
+      return res.status(400).json({ "message": "These are required fields" });
+    }
+    
+    const reqProject = await Project.findOne({ "_id": projectId });
+    if (reqProject?.user._id.toString() !== userId?.toString()) {
+      return res.status(401).json({
+        "success": "false",
+        "message": "You are not authorized for this"
+      })
+    }
+
+    const foundProject = await Project.findOneAndUpdate({ "_id": projectId }, {
+      title,
+      description,
+      tags,
+      githubLink,
+      liveLink,
+    });
+    return res.status(201).json({
+      "success": "true",
+      "message": "Project updated successfully!",
+      foundProject,
+    })
+  } catch (error) {
+    console.log(`${error}`);
+    throw new Error(`While editing the blog`);
+  }
+}
--- backend/routes/project.ts
+++ backend/routes/project.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import { getAllProjects, postAProject, getProjectById, deleteProjectById, editProjectById } from "../controllers/project.ts"
+import { getAllProjects, postAProject, getProjectById, deleteProjectById, editProjectById } from "../controllers/project"
 
 export const projectRoute = express.Router()
 
-projectRoute.get("/", getAllProjects);    // get all the posted getAllProjects
-projectRoute.post("/", postAProject);     // posting a project 
-projectRoute.get("/:projectId", getProjectById);   // get a particular project
-projectRoute.delete("/delete/:projectId", deleteProjectById);  // delete a project (auth)
-projectRoute.put("/edit/:projectId", editProjectById);         // edit a project (auth)
-
+projectRoute.get("/:projectId", getProjectById);                    // get a particular project
+projectRoute.delete("/delete/:projectId", deleteProjectById);       // delete a project (auth)
+projectRoute.put("/edit/:projectId", editProjectById);              // edit a project (auth)
+projectRoute.get("/", getAllProjects);                              // get all the posted getAllProjects
+projectRoute.post("/post", postAProject);                           // posting a project 
 
