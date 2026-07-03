ba85d881734ef05183ff691352b42651b1b45233
ayush00git <ayush.2007.iit@gmail.com>
2026-07-03 22:25:06 +0530

    fix: delete stale dir/

--- backend/routes/feature.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { allowOnlyAuthenticatedUser } from "../middlewares/auth";
-import { getFeatures, postFeatures } from "../controllers/feature";
-
-export const featureRoute = express.Router();
-
-featureRoute.get('/', getFeatures);
-featureRoute.post('/', allowOnlyAuthenticatedUser, postFeatures);
-
